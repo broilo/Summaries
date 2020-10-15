2701be44d1a163c8a33f91d4b8e4b87bb66b01a8
Mateus Broilo <mateus.broilo90@gmail.com>
2020-10-15 10:23:04 -0300

    add: update example-2.py

---
+++ linear-programming/example-2.py
@@ -0,0 +1,29 @@
+# Variable and Equation Arrays
+# x1=x0+p, p=1.2
+# x2-1=x1+x0
+# x2=x1**2
+
+from gekko import GEKKO
+
+# Initialize model
+m = GEKKO()
+
+# Define parameter
+p = m.Param(1.2)
+
+# Define array
+x = m.Array(m.Var, 3)
+
+# Equations
+eq0 = x[1] == x[0]+p
+eq1 = x[2]-1 == x[1]+x[0]
+eq2 = x[2] == x[1]**2
+m.Equation([eq0, eq1, eq2])
+
+# Solve simmulation
+m.solve()
+#m.solve(disp=False)
+
+# Results
+for i in range(3):
+    print(f"x[{i}]={x[i].value}")
