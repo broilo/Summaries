887d60921b875957b7ed7ba26d4c4cdf2b59b86d
Mateus Broilo <mateus.broilo90@gmail.com>
2020-10-15 10:11:14 -0300

    gen: create example-1.py

---
+++ linear-programming/example-1.py
@@ -0,0 +1,24 @@
+# Solve linear equation
+# 3x+2y=1
+# x+2y=0
+
+from gekko import GEKKO
+
+# Initialize model
+m = GEKKO()
+
+# Define variables
+x = m.Var()
+y = m.Var()
+
+# Equations
+m.Equation(3*x+2*y == 1)
+m.Equation(x+2*y == 0)
+
+# Solve simmulation
+m.solve()
+#m.solve(disp=False)
+
+# Results
+print("")
+print(f"x: {x.value} and y: {y.value}")
