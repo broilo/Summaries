7bb96a82e830128e960e75831b11e721f0d70ea8
Mateus Broilo <mateus.broilo90@gmail.com>
2020-10-26 16:11:52 -0300

    gen: create lin_cl.py

--- Hands-on_ML/chap4/lin_cl.py
+++ Hands-on_ML/chap4/lin_cl.py
@@ -1,23 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 np.random.seed(42)
 
 X = 2*np.random.rand(100, 1)
 y = 4+3*X+np.random.randn(100, 1)
 
-X_b = np.c_[np.ones((100, 1)), X]
-theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
-
-theta_best
+lin_cl = LinearRegression()
+lin_cl.fit(X, y)
+lin_cl.intercept_, lin_cl.coef_
 
 X_new = np.array([[0], [2]])
-X_new_b = np.c_[np.ones((2, 1)), X_new]
-y_predict = X_new_b.dot(theta_best)
 
-y_predict
+y_predict = lin_cl.predict(X_new)
 
 plt.plot(X_new, y_predict, "r-")
 plt.plot(X, y, "b.")
 plt.axis([0, 2, 0, 15])
 plt.show()
+
