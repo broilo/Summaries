06fff17789ab45f702df8f6a5867df95d6e18755
Mateus Broilo <mateus.broilo90@gmail.com>
2020-10-29 16:26:15 -0300

    gen: create lin_reg_gradient-descent.py

---
+++ Hands-on_ML/chap4/lin_reg_gradient-descent.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+
+X = 2*np.random.rand(100, 1)
+y = 4+3*X+np.random.randn(100, 1)
+
+X_b = np.c_[np.ones((100, 1)), X]
+
+eta = 0.1  # learning rate
+n_iterations = 1000
+m = 100
+
+theta = np.random.randn(2, 1)  # random initialization
+
+for iteration in range(n_iterations):
+    gradients = 2/m * X_b.T.dot(X_b.dot(theta)-y)
+    theta = theta - eta * gradients
+
+theta
+
+X_new = np.array(([0], [2]))
+X_new_b = np.c_[np.ones((2, 1)), X_new]
+
+X_new_b.dot(theta)
+
+theta_path_bgd = []
+
+def plot
+
+plt.plot(X, y, "b.")
+plt.axis([0, 2, 0, 15])
+plt.show()
