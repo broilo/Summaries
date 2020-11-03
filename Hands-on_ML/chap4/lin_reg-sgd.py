e9c891d038178db267333a082fc2342c708479a5
Mateus Broilo <mateus.broilo90@gmail.com>
2020-11-03 15:22:06 -0300

    add: update lin_reg-sgd.py chap4

--- Hands-on_ML/chap4/lin_reg-sgd.py
+++ Hands-on_ML/chap4/lin_reg-sgd.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 np.random.seed(42)
 
@@ -8,6 +9,7 @@ y = 4+3*X+np.random.randn(100, 1)
 X_b = np.c_[np.ones((100, 1)), X]
 
 X_new = np.array(([0], [2]))
+X_new_b = np.c_[np.ones((2, 1)), X_new]
 
 n_epochs = 50
 t0, t1 = 5, 50  # learning schedule hyperparameter
@@ -20,13 +22,24 @@ def learning_schedule(t):
 
 theta = np.random.randn(2, 1)  # random initialization
 
+theta_path_sgd = []
+
 for epoch in range(n_epochs):
     for i in range(m):
+        if epoch == 0 and i < 20:
+            y_predict = X_new_b.dot(theta)
+            style = "b-" if i > 0 else "r--"
+            plt.plot(X_new, y_predict, style)
         random_index = np.random.randint(m)
         xi = X_b[random_index:random_index + 1]
         yi = y[random_index:random_index + 1]
         gradients = 2 * xi.T.dot(xi.dot(theta)-yi)
         eta = learning_schedule(epoch * m + i)
         theta = theta - eta * gradients
-
-theta
+        theta_path_sgd.append(theta)
+    
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([0, 2, 0, 15])
+plt.show()
