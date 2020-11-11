dc9518b0c24fb769d60e6ea99b3c20bb0b70b270
Mateus Broilo <mateus.broilo90@gmail.com>
2020-11-11 17:02:03 -0300

    add: update elastic_net-regularization.py chap4 -> ElasticNet

--- Hands-on_ML/chap4/elastic_net-regularization.py
+++ Hands-on_ML/chap4/elastic_net-regularization.py
@@ -9,6 +9,6 @@ X = 3*np.random.rand(m, 1)
 y = 1+0.5*X+np.random.randn(m, 1)/1.5
 X_new = np.linspace(0, 3, 100).reshape(100, 1)
 
-lasso_reg = Lasso(alpha=0.1)
-lasso_reg.fit(X, y)
-lasso_reg.predict([[1.5]])
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic_net.fit(X, y)
+elastic_net.predict([[1.5]])
